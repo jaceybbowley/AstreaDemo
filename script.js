07f587f264fea7441d773a5022ee290003191fab
jaceybbowley <jacey.bowley@pm.me>
2023-02-27 15:21:48 -0700

    Redirect from login

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-//const form = document.querySelector('form');
-//const usernameInput = document.getElementById('username');
-//const passwordInput = document.getElementById('password');
-//
-//form.addEventListener('submit', (e) => {
-//  e.preventDefault();
-//
-//  const usernameValue = usernameInput.value.trim();
-//  const passwordValue = passwordInput.value.trim();
-//
-//  if (usernameValue === '' || passwordValue === '') {
-//    alert('Please fill in all fields');
-//  } else if ((usernameValue === 'user1' && passwordValue === 'pass1') || (usernameValue === 'user2' && passwordValue === 'pass2')) {
-//    alert('Login successful');
-//  } else {
-//    alert('Incorrect username or password');
-//  }
-//});
-//
-//const button = document.getElementById('button');
-//
-//button.addEventListener('click', () => {
-//  alert('Hello, World!');
-//});
-
 const loginForm = document.getElementById('login-form');
 
 loginForm.addEventListener('submit', (event) => {
@@ -33,10 +8,10 @@ loginForm.addEventListener('submit', (event) => {
   const password = document.getElementById('password').value;
 
   // Check if username and password are correct (for demo purposes only)
-  if (username === 'user1' && password === 'pass1') {
+  if (username === 'user1' && password === 'password1') {
     // Redirect to homeA page
     window.location.href = 'homeA.html';
-  } else if (username === 'user2' && password === 'pass2') {
+  } else if (username === 'user2' && password === 'password2') {
     // Redirect to homeB page
     window.location.href = 'homeB.html';
   } else {
@@ -44,4 +19,3 @@ loginForm.addEventListener('submit', (event) => {
     alert('Incorrect username or password!');
   }
 });
-
